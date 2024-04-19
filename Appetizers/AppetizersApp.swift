989faedbe03fcf4e83fef7af45f316d3016b1f41
xizana <xizoooo102@gmail.com>
2024-04-19 10:47:12 +0400

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Lasha Khizanishvili on 19.04.24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
