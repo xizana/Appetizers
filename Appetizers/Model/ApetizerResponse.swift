69b40e593a4f143e2c0b70067a057b19b44b6e9c
xizana <xizoooo102@gmail.com>
2024-04-19 13:53:29 +0400

    add model for response.

---
+++ Appetizers/Model/ApetizerResponse.swift
@@ -0,0 +1,31 @@
+//
+//  ApetizerResponse.swift
+//  Appetizers
+//
+//  Created by Lasha Khizanishvili on 19.04.24.
+//
+
+import Foundation
+
+
+struct ApetizerResponse: Codable {
+    let request: [Apetizer]
+}
+
+struct Apetizer: Codable {
+    let id: Int
+    let imageURL: String
+    let description: String
+    let calories: Int
+    let price: Double
+    let carbs: Int
+    let name: String
+}
+
+
+struct MockData {
+    
+    static let apetizerTemp = Apetizer(id: 4, imageURL: "sdadad", description: "asdadadadadas", calories: 33, price: 22, carbs: 12, name: "asdadadada")
+    
+    static let apetizers = [apetizerTemp, apetizerTemp, apetizerTemp, apetizerTemp]
+}
