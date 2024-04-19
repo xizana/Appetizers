7bb7c9be89d9d212736c28fa67c03bcc3ffad87e
xizana <xizoooo102@gmail.com>
2024-04-19 11:14:44 +0400

    add tabbar items.

--- Appetizers/AppetizerTabView.swift
+++ Appetizers/AppetizerTabView.swift
@@ -10,8 +10,22 @@ import SwiftUI
 struct AppetizerTabView: View {
     var body: some View {
         TabView {
-            Text("Hello, world!")
-                .padding()
+            AppetizersListView()
+                .tabItem {
+                Image(systemName: "house")
+                Text("Home")
+            }
+            AccountView()
+                .tabItem {
+                Image(systemName: "person")
+                Text("Account")
+            }
+            
+            OrderView()
+                .tabItem {
+                Image(systemName: "bag")
+                Text("Order")
+            }
         }
     }
 }
--- Appetizers/Modules/HomeModule/AppetizersListView.swift
+++ Appetizers/Modules/HomeModule/AppetizersListView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct HomeView: View {
+struct AppetizersListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("home view")
     }
 }
 
 #Preview {
-    HomeView()
+    AppetizersListView()
 }
